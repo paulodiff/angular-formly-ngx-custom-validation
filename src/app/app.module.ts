8b54d65ece0e81a8017522f7a57114ccf41f56a7
paulodiff <paulo.difficiliora@gmail.com>
2020-05-23 14:11:41 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,11 +21,25 @@ export function IpValidator(control: FormControl , field: FormlyFieldConfig): Va
   // console.log("1)", control.parent);
   // console.log("2)", control.parent.controls["firstName"]);
   // console.log("3)", control.parent.controls["firstName"].parent);
-  return !control.value || /(\d{1,3}\.){3}\d{1,3}/.test(control.value) ? null : { 'ip': true };
+  let maxValue = parseInt(field.templateOptions.maxValue);
+  let minValue = parseInt(field.templateOptions.minValue);
+  let curValue = parseInt(control.value);
+  console.log(curValue, minValue, maxValue);
+  let bValid = { 'ip': true };
+  if ((curValue < maxValue) && (curValue > minValue)) {
+    bValid = null;
+  } else {
+    bValid = { 'ip': true };
+  }
+  // return !control.value || /(\d{1,3}\.){3}\d{1,3}/.test(control.value) ? null : { 'ip': true };
+  console.log(control.value);
+  console.log(bValid);
+  return bValid;
+
 }
 
 export function IpValidatorMessage(err, field: FormlyFieldConfig) {
-  return `"${field.formControl.value}" is not a valid IP Address`;
+  return `"${field.formControl.value}" is not in RANGE`;
 }
 
 @NgModule({
