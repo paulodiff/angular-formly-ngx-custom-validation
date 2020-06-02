74b9c36c1ed1284fa9dbbaab7835c5810f7ff2c7
paulodiff <paulo.difficiliora@gmail.com>
2020-06-02 13:08:39 +0200

    2 files modified

--- src/app/json/json.component.ts
+++ src/app/json/json.component.ts
@@ -42,6 +42,7 @@ export class JsonComponent implements OnInit, OnDestroy {
        // In a real app: dispatch action to load the details here.
       this._appService.getFormData(this.itemId).subscribe(([model, fields]) => {
         console.log('returned...');
+        // this.options = {};
         this.model = {};
         this.fields = null;
         this.model = model;
