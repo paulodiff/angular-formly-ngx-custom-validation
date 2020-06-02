4eb9295465900f7cc35943e87c371d909016d6a2
paulodiff <paulo.difficiliora@gmail.com>
2020-06-02 17:44:07 +0200

    1 file modified

--- src/app/services/validator.ts
+++ src/app/services/validator.ts
@@ -59,6 +59,26 @@ export function minMaxValidatorMessage(err, field: FormlyFieldConfig) {
   return `"${field.formControl.value}" is not in RANGE`;
 }
 
+// date format Validator
+/**
+ * 
+ * 
+ ^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$
+ * 
+ * 
+ */
+
+export function dateValidator(control: FormControl , field: FormlyFieldConfig): 
+ValidationErrors {
+  console.log("dateValidator");
+   return !control.value || /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/.test(control.value) ? null : { 'dateValidator': true };
+}
+
+export function dateValidatorMessage(err, field: FormlyFieldConfig) {
+  return `"${field.formControl.value}" non è nel formato corretto gg/mm/aaaa (3) !`;
+}
+
+
 // email Validator
 // /^\S+@\S+\.\S+$/
 
