ec408ce74d645c50691e58856ce2a36158f462dc
paulodiff <paulo.difficiliora@gmail.com>
2020-06-02 13:02:56 +0200

    2 files modified

--- src/app/services/validator.ts
+++ src/app/services/validator.ts
@@ -11,7 +11,7 @@ ValidationErrors {
 }
 
 export function codiceFiscaleValidatorMessage(err, field: FormlyFieldConfig) {
-  return `"${field.formControl.value}" non è nel formato corretto (1) !`;
+  return `"${field.formControl.value}" non è nel formato corretto (cf1) !`;
 }
 
 // /^(?:[A-Z][AEIOU][AEIOUX]|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$/i
@@ -23,7 +23,7 @@ ValidationErrors {
 }
 
 export function codiceFiscaleValidator2Message(err, field: FormlyFieldConfig) {
-  return `"${field.formControl.value}" non è nel formato corretto (2) !`;
+  return `"${field.formControl.value}" non è nel formato corretto (cf2) !`;
 }
 
 export function minMaxValidator(control: FormControl , field: FormlyFieldConfig): ValidationErrors {
