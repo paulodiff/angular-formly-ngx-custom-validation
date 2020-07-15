d6f8971c62402ed3c505be7a4fd3517f3174705c
paulodiff <paulo.difficiliora@gmail.com>
2020-07-15 10:30:53 +0200

    2 files modified, 4 added

---
+++ src/app/formly/svg-captcha/formly-svgcaptcha-type.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FieldType } from '@ngx-formly/core';
+
+@Component({
+  selector: 'svgcaptcha',
+  template: `
+    <div class="card"><div class="card-body">
+
+    <p class="u-text-h3">{{to.title}}</p>
+    <p>{{formState.svgcaptcha.image}}</p>
+
+    <img [src]="'data:image/svg+xml;base64,' + to.svgImageB64 | safeHtml ">
+    <p>{{to.description}}</p>
+    <p>{{id}}</p>
+    <input
+    type="text"
+    [formControl]="formControl"
+    [formlyAttributes]="field"
+    [class.is-invalid]="showError"
+    >
+
+    </div></div>
+  `,
+})
+export class FormlySvgCaptchaFieldType extends FieldType {}
+
+// <img [src]="{{to.svgImage | safeHtml}}" class="u-sizeFit">
+// "data:image/svg+xml;base64,"
