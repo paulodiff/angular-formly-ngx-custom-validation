a924bb2562a6963310a2831e3271549e8c81f090
paulodiff <paulo.difficiliora@gmail.com>
2020-08-19 13:20:22 +0200

    1 file modified, 1 added

---
+++ src/app/formly/auth-info/formly-auth-info-type.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FieldType } from '@ngx-formly/core';
+
+@Component({
+  selector: 'auth-info',
+  template: `
+    
+
+   
+   <h1>Auth Info</h1>
+   
+    <img [src]="'data:image/svg+xml;;utf8,' + formState.svgcaptcha.svgImage | safeHtml ">
+    <p>{{formState.svgcaptcha.question}}</p>
+   
+    <input
+    type="text"
+    [formControl]="formControl"
+    [formlyAttributes]="field"
+    [class.is-invalid]="showError"
+    >
+
+
+ 
+    
+  `,
+})
+export class FormlyAuthInfoFieldType extends FieldType {}
+
+// <img [src]="{{to.svgImage | safeHtml}}" class="u-sizeFit">
+// <img [src]="'data:image/svg+xml;base64,' + to.svgImageB64 | safeHtml ">
+// "data:image/svg+xml;base64,"
