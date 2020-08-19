c30cc05b6d2df49527873e9fcb901d54b3c68639
paulodiff <paulo.difficiliora@gmail.com>
2020-08-19 14:50:12 +0200

    3 files modified

--- src/app/formly/authinfo/formly-authinfo-type.component.ts
+++ src/app/formly/authinfo/formly-authinfo-type.component.ts
@@ -4,20 +4,40 @@ import { FieldType } from '@ngx-formly/core';
 @Component({
   selector: 'authinfo',
   template: `
-    
+     
+  
+
+    <table class="table table-bordered table-sm">
+    <tr>
+      <td>Nome:</td>
+      <td>{{formState.security.utenteNome}}</td>
+    </tr>
+    <tr>
+      <td>Cognome:</td>
+      <td>{{formState.security.utenteCognome}}</td>
+    </tr>
+     <tr>
+      <td>Codice Fiscale:</td>
+      <td>{{formState.security.utenteCodiceFiscale}}</td>
+    </tr>
+     <tr>
+      <td>Data di nascita:</td>
+      <td>{{formState.security.utenteDataDiNascita}}</td>
+    </tr>
+       <tr>
+      <td>Origine autenticazione:</td>
+      <td>{{formState.security.sourceAuth}}</td>
+    </tr>
+    </table>
+   
+<div class="alert alert-danger" role="alert">
+  N.B. nel caso le informazioni di autenticazioni non fossero corrette cliccare immediatamente sul bottone seguente:
+  <button class="btn btn-primary btn-block">ESCI DAL SEGUENTE MODULO</button>
+</div>
+
 
    
-   <h1>Auth Info</h1>
-   
-  
-    <p>{{formState.security.token}}</p>
-   
-    <input
-    type="text"
-    [formControl]="formControl"
-    [formlyAttributes]="field"
-    [class.is-invalid]="showError"
-    >
+ 
 
 
  
