b2dfa5b0fa94b82731634b534948412c40d3d812
paulodiff <paulo.difficiliora@gmail.com>
2020-08-29 08:57:23 +0200

    3 files modified

--- src/app/formly/formly-repeat-section.type.ts
+++ src/app/formly/formly-repeat-section.type.ts
@@ -4,16 +4,16 @@ import { FieldArrayType } from '@ngx-formly/core';
 @Component({
   selector: 'formly-repeat-section',
   template: `
-    FG: <pre>{{ to.fieldGroup }}</pre>
-    <div *ngFor="let field of to.fieldGroup; let i = index;" class="row">
-      <formly-field class="col" [field]="field"></formly-field>
-      <div class="col-sm-2 d-flex align-items-center">
+    <hr/>
+    <div *ngFor="let field of field.fieldGroup; let i = index;">
+      <formly-group [field]="field">
+    <div class="col-sm-2 d-flex align-items-center">
         <button class="btn btn-danger" type="button" (click)="remove(i)">Remove</button>
       </div>
+      </formly-group>
     </div>
     <div style="margin:30px 0;">
-      <button class="btn btn-primary" type="button" (click)="add()">{{ to.addText }}</button>
-    </div>
+      <button class="btn btn-primary" type="button" (click)="add()">Add</button>
   `,
 })
 export class FormlyRepeatTypeComponent extends FieldArrayType {}
